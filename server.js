fef108414521a34b4ee7d0e72962396c28102187
rthomase422 <rthomase@live.unc.edu>
2022-03-26 12:15:19 -0400

    server change

--- server.js
+++ server.js
@@ -5,15 +5,21 @@ var port = 5000
 
 const server = app.listen(port, () => {
     console.log('App is running on port %PORT%'.replace('%PORT%',port))
-})
+});
+function coinFlip() {
+    return Math.random() > .5 ? ("heads") : ("tails")
+}
+
 app.get('/app', (req, res) => {
     res.status(200).end('OK')
     res.type('text/plain')
 
 })
-app.get('/app/echo/:number', (req,res) => {
-    res.status(200).json({ 'message': req.params.number})
+app.get('/app/flip', (req, res) => {
+    var flip = coinFlip()
+    res.status(200).json({ 'flip' : flip})
 })
+
 
 app.use(function(req, res) {
     res.status(404).end("Endpoint does not exist")
