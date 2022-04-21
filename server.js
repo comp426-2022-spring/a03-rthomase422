ddeb7e48796af3cbcedccd17d6a586ae1895ca0d
rthomase422 <rthomase@live.unc.edu>
2022-04-21 17:32:49 -0400

    functions added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-var port = 5000
+//var port = 5000
 
 const server = app.listen(port, () => {
     console.log('App is running on port %PORT%'.replace('%PORT%',port))
@@ -10,14 +10,29 @@ function coinFlip() {
     return Math.random() > .5 ? ("heads") : ("tails")
 }
 
-app.get('/app', (req, res) => {
+app.get('/app/', (req, res) => {
     res.status(200).end('OK')
     res.type('text/plain')
 
 })
-app.get('/app/flip', (req, res) => {
+app.get('/app/flip/', (req, res) => {
     var flip = coinFlip()
-    res.status(200).json({ 'flip' : flip})
+    res.status(200).json({ "flip" : flip})
+})
+
+app.get('/app/flips/:number', (req, res) => {
+    var flips = coinFlips(req.params.number)
+    res.status(200).json({ "raw" : flips, "summary" : countFlips(flips)})
+})
+
+app.get('/app/flip/call/heads', (req, res) => {
+    var heads = flipACoin("heads")
+    res.status(200).json(heads)
+})
+
+app.get('/app/flip/call/tails', (req, res) => {
+    var tails = flipACoin("tails")
+    res.status(200).json(tails)
 })
 
 
@@ -25,3 +40,48 @@ app.use(function(req, res) {
     res.status(404).end("Endpoint does not exist")
     res.type("text/plain")
 })
+
+function coinFlip() {
+    return Math.random() > .5 ? ("heads") : ("tails")
+}
+
+function coinFlips(flips) {
+    var temp = new Array();
+    if (flips < 1 || typeof flips == 'undefined') {
+      flips = 1;
+    }
+    for (var i = 0; i < flips; i++) {
+      //var coin = Math.random > .5 ? ("heads") : ("tails");
+  
+      temp.push(coinFlip());
+    }
+    return temp;
+}
+
+function countFlips(array) {
+    let h = 0;
+    let t = 0;
+    for (let i  = 0; i < array.length; i++) {
+      if (array[i] == 'heads') {
+        h++;
+      }
+      if (array[i] == 'tails') {
+        t++;
+  
+      }
+    }
+    return {heads: h, tails: t};
+   
+}
+
+function flipACoin(call) {
+    let flip = coinFlip()
+    let result = '';
+    if (flip == call) {
+      result = 'win';
+    } else {
+      result = 'lose';
+    }
+    return {call: call, flip: flip, result: result}
+}
+  
