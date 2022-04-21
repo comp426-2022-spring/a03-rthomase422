5543d0cd249c5e3a55cac95475d3dfdbf87c1b1c
rthomase422 <rthomase@live.unc.edu>
2022-04-21 17:42:19 -0400

    hiiiiiiii

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express')
 const app = express()
-
-//var port = 5000
+const args = require('minimist')(process.argv.slice(2))
+args['port']
+const port = args.port || process.env.PORT || 5000
 
 const server = app.listen(port, () => {
     console.log('App is running on port %PORT%'.replace('%PORT%',port))
 });
-function coinFlip() {
-    return Math.random() > .5 ? ("heads") : ("tails")
-}
 
 app.get('/app/', (req, res) => {
     res.status(200).end('OK')
